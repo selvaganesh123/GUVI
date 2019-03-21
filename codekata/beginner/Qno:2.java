3f6c607cd00b959fa44fa7d834a54fd7b3f3cb31
selvaganesh123 <36336261+selvaganesh123@users.noreply.github.com>
2019-03-21 10:41:56 +0530

    Update Qno:2.java

--- codekata/beginner/Qno:2.java
+++ codekata/beginner/Qno:2.java
@@ -5,8 +5,8 @@ public class Qno:2
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int num=s.nextInt();
-		if(num%2==0){
-		    System.out.println("Even");
+		if(num==0){
+		    System.out.println("Zero");
 		}
 		else if(num<0){
 		    System.out.println("Invalid");
@@ -14,8 +14,8 @@ public class Qno:2
 		else if(num%2!=0){
 		    System.out.println("Odd");
 		}
-		else if(num==0){
-		    System.out.println("Zero");
+		else if(num%2==0){
+		    System.out.println("Even");
 		}
 	}
 }
