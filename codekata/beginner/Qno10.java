70b8eba9a75efb18fab913fae10fd898e3e27c66
selvaganesh123 <36336261+selvaganesh123@users.noreply.github.com>
2019-08-20 23:02:43 +0530

    Create Qno10.java

---
+++ codekata/beginner/Qno10.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.io.*;
+public class Example {
+public static void main(String[] args) {
+Scanner s=new Scanner(System.in);
+       System.out.println("enter the number:");
+       int n=s.nextInt();
+       int total=0;
+       while(n>0){
+           n=n/10;
+           total=total+1;
+       }System.out.println("total number is "+total);
+    }
+    
+}
