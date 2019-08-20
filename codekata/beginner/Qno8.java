55d2687efd76ac36513f68958de692285285672a
selvaganesh123 <36336261+selvaganesh123@users.noreply.github.com>
2019-08-20 23:45:55 +0530

    Update Qno8.java

--- codekata/beginner/Qno8.java
+++ codekata/beginner/Qno8.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 import java.io.*;
-public class Example {
+public class Qno8 {
 public static void main(String[] args) {
 Scanner s=new Scanner(System.in);
        System.out.println("enter the number");
