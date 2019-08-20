af060e82102a4f7cab9a1a455be81b2100d75ffb
selvaganesh123 <36336261+selvaganesh123@users.noreply.github.com>
2019-08-20 23:45:26 +0530

    Update Qno10.java

--- codekata/beginner/Qno10.java
+++ codekata/beginner/Qno10.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 import java.io.*;
-public class Example {
+public class Qno10 {
 public static void main(String[] args) {
 Scanner s=new Scanner(System.in);
        System.out.println("enter the number:");
